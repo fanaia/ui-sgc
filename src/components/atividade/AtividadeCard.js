69874c91724ab1a6b4956a4f4315fac81444f5b1
Lucas Ribeiro <lucasr.camargo@live.com>
2024-06-03 16:00:13 -0300

    refactor: Update AtividadeCard component to handle status colors and card opacity

--- src/components/atividade/AtividadeCard.js
+++ src/components/atividade/AtividadeCard.js
@@ -5,6 +5,16 @@ import CrudContext from "../../contexts/CrudContext";
 const AtividadeCard = ({ item }) => {
   const { setSelectedItem, setShowModal } = useContext(CrudContext);
 
+  const statusColors = {
+    ativo: "green",
+    pendente: "yellow",
+    cancelado: "red",
+    recusado: "red",
+  };
+
+  const cardOpacity =
+    item?.status === "cancelado" || item?.status === "recusado" ? 0.7 : 1;
+
   const handleSelect = () => {
     setSelectedItem(item);
     setShowModal(true);
@@ -15,9 +25,9 @@ const AtividadeCard = ({ item }) => {
       key={item._id}
       style={{
         width: "100%",
-        margin: "10px",
+        margin: "10px auto",
         cursor: "pointer",
-        borderLeft: `10px solid ${item.corEtiqueta}`,
+        opacity: cardOpacity,
       }}
       onClick={handleSelect}
     >
@@ -30,11 +40,11 @@ const AtividadeCard = ({ item }) => {
           {item?.grupoTrabalho ? item.grupoTrabalho.nome : "Carregando..."}
         </Card.Text>
         <Card.Text>
-          <strong>Projeto</strong>
+          <strong>Projeto: </strong>
           {item?.projeto ? item.projeto.nome : "Carregando..."}
         </Card.Text>
         <Card.Text>
-          <strong>Participante</strong>
+          <strong>Participante: </strong>
           {item?.participante ? item.participante.nome : "Carregando..."}
         </Card.Text>
         <Card.Text>
@@ -42,6 +52,13 @@ const AtividadeCard = ({ item }) => {
           {item?.status}
         </Card.Text>
       </Card.Body>
+      <Card.Footer
+        style={{
+          height: "8px",
+          padding: "0",
+          backgroundColor: statusColors[item?.status] || "grey",
+        }}
+      />
     </Card>
   );
 };
