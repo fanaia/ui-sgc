c7aca5aa714b567a4428527f69d72d85e8ae4b64
Fabio <90531115+fanaia@users.noreply.github.com>
2024-06-04 19:54:26 -0300

    ajustes

--- src/components/cardsIndicadores/CardSaldoTokensParticipante.js
+++ src/components/cardsIndicadores/CardSaldoTokensParticipante.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import apiRetaguarda from "../../config/apiRetaguarda";
-import {  formatToken } from '../../utils/displayFormatters';
+import { formatToken } from "../../utils/displayFormatters";
 
 const CardSaldoTokensParticipante = () => {
   const [saldoTotal, setSaldoTotal] = useState(0);
 
   useEffect(() => {
     const fetchSaldoTotal = async () => {
-      const response = await apiRetaguarda.get('tokens/saldo-participante');
+      const response = await apiRetaguarda.get("tokens/saldo-participante");
       setSaldoTotal(response.data.saldo);
     };
 
@@ -19,9 +19,7 @@ const CardSaldoTokensParticipante = () => {
     <Card className="mb-3">
       <Card.Header>Meu Saldo</Card.Header>
       <Card.Body>
-        <Card.Text>
-          {formatToken(saldoTotal)}
-        </Card.Text>
+        <Card.Text>{formatToken(saldoTotal)}</Card.Text>
       </Card.Body>
     </Card>
   );
