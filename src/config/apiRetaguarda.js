48f99beb766ac99fdf407cd8b0fe509ad6c773fc
Fabio <90531115+fanaia@users.noreply.github.com>
2024-06-05 22:06:49 -0300

    ajuste .env

--- src/config/apiRetaguarda.js
+++ src/config/apiRetaguarda.js
@@ -4,6 +4,8 @@ const apiRetaguarda = axios.create({
   baseURL: process.env.MS_RETAGUARDA,
 });
 
+console.log("process.env.MS_RETAGUARDA", process.env.REACT_APP_MS_RETAGUARDA)
+
 apiRetaguarda.interceptors.request.use((config) => {
   const tokenJwt = localStorage.getItem("tokenJwt");
   if (tokenJwt) {
