89783a1b9ce642807fd188eee1760d6552b3ff6d
Fabio <90531115+fanaia@users.noreply.github.com>
2024-06-08 16:32:22 -0300

    chore: Update package dependencies and add react-hook-form for form handling

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,24 +1,66 @@
-import { useState } from "react";
-import { Container, FormControl } from "react-bootstrap";
-import { Form } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import apiRetaguarda from "axios";
 
 const StartPage = () => {
-  const [seed, setSeed] = useState();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    apiRetaguarda
+      .post("/createSeed", data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <Container>
       <h1>Semente</h1>
-
-      <Form>
-        <Form.Group>
-          <Form.Label>Nome do Projeto</Form.Label>
-          <FormControl
-            type="text"
-            value={seed?.nome}
-            onChange={(e) => setSeed({ ...seed, nome: e.target.value })}
-          />
-        </Form.Group>
-      </Form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>contratoSocial</label>
+          <input {...register("seed.contratoSocial")} />
+        </div>
+        <div>
+          <label>tokenNome</label>
+          <input {...register("seed.tokenNome")} />
+        </div>
+        <div>
+          <label>tokenSigla</label>
+          <input {...register("seed.tokenSigla")} />
+        </div>
+        <div>
+          <label>liquidacaoMinima</label>
+          <input {...register("seed.liquidacaoMinima")} />
+        </div>
+        <div>
+          <label>ativarEstagioParticipante</label>
+          <input {...register("seed.ativarEstagioParticipante")} />
+        </div>
+        <div>
+          <h2>participante</h2>
+          <div>
+            <label>nome</label>
+            <input {...register("seed.participante.nome")} />
+          </div>
+          <div>
+            <label>email</label>
+            <input {...register("seed.participante.email")} />
+          </div>
+          <div>
+            <label>whatsapp</label>
+            <input {...register("seed.participante.whatsapp")} />
+          </div>
+          <div>
+            <label>senha</label>
+            <input {...register("seed.participante.senha")} />
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </Container>
   );
 };
